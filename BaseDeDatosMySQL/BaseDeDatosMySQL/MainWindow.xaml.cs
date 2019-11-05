028da3e6c2766051f199d6beb3a387b130b90c64
Alumno <Alumno@PC22>
2019-11-05 10:05:38 -0600

    DB con SQLite

---
+++ BaseDeDatosMySQL/BaseDeDatosMySQL/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BaseDeDatosMySQL.Clases;  //aqui va el nombre de tu proyecto, como la nombramos
+using SQLite;   //agregamos dos librerias de conexion de base de datos
+
+namespace BaseDeDatosMySQL
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuardar_Click(object sender, RoutedEventArgs e)
+        {
+            Contacto contacto = new Contacto()
+            {
+                Nombre = txtNombre.Text,
+                Correo = txtCorreo.Text,
+                Telefono = txtTelefono.Text
+            };
+            using (SQLiteConnection conexion = new SQLiteConnection(App.databasePath))
+            {
+                conexion.CreateTable<Contacto>();
+                conexion.Insert(contacto);
+            }
+            
+        }
+    }
+}
